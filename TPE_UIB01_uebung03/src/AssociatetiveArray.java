cb5e8b7ac62bc4ffba821e2d48d55efc68219bad
Max <Max>
2014-11-15 16:30:23 +0100

    initialize

---
+++ TPE_UIB01_uebung03/src/AssociatetiveArray.java
@@ -0,0 +1,18 @@
+
+public interface AssociatetiveArray {
+
+	public void clear();
+	public boolean conatiansValue();
+	public boolean containsKey(String key);
+	public int get(String key);
+	public boolean isEmpty();
+	public void put(String key);
+	public void putAll(String[] array);
+	public int remove(String key);
+	public int size();
+	public int update(int amount, String key);
+	public void forEach();
+	public void extractAll(String[]array);
+	public void map();
+	
+}
