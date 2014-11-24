823824193424425ac09c237fe4efa0c26be1cd55
Sovann <sovann-som@live.de>
2014-11-24 11:54:50 +0100

    änderung methoden

--- TPE_UIB01_uebung03/src/ansatz/BinärHashBaum.java
+++ TPE_UIB01_uebung03/src/ansatz/BinärHashBaum.java
@@ -2,16 +2,14 @@ package ansatz;
 
 import java.util.Iterator;
 
+public class BinärHashBaum<K, V> implements AssociativeArray<K, V> {
 
-	public class BinärHashBaum <K, V> implements AssociativeArray<K, V> {
-		
-		
+	private Node<K, V> root;
 
-		private Node<K,V> root;
-		public BinärHashBaum(K key, V value) {
-			this.root = new Node<K,V> (key, value);
-		}
-		
+	public BinärHashBaum(K key, V value) {
+		this.root = new Node<K, V>(key, value);
+	}
+
 	public class Node<K, V> implements Comparable {
 
 		private K key;
@@ -73,169 +71,156 @@ import java.util.Iterator;
 		}
 	}
 
-		
-		@Override
-		public void clear() {
-			root = null;
-			
-		}
+	@Override
+	public void clear() {
+		root = null;
 
-		@Override
-		public boolean containsValue(V value, Node<K,V> node) {
-			if (node == null) {
-				return false;
-			}
-			else {
-				if (node.value == value) {
-					return true;
-				}
-					return containsValue(value, node.left) &&
-					containsValue(value, node.right);
-			}	
-		}
-		
-		
-		@Override
-		public boolean containsKey(int hashKey, Node node) {
-			if (node == null) {
-				return false;
-			}
-			else {
-				if (node.hashKey == hashKey) {
-					return true;
-				}
-				else if (hashKey < node.hashKey) {
-					return containsKey(hashKey, node.left);
-				}
-				else {
-					return containsKey(hashKey, node.right);
-				}
-			}	
-		}
+	}
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public V get(K key, Node node) {
-			if (node == null) {
-				return null;
-			}
-			else {
-				if (node.key == key) {
-					return (V) node.value;
-				}
-				else if (key.hashCode() < node.key.hashCode()) {
-					return get(key, node.left);
-				}
-				else {
-					return get(key, node.right);
-				}
-			}	
-		}
-
-		@Override
-		public boolean isEmpty() {
-			return root == null;
-		}
-
-		@Override
-		public void put(K key, V value) {  
-			root = put(root, key, value);
-		}
-
-		
-		public Node put(Node x, K key, V value) {
-			if (x == null) { 
-				return new Node(key, value);
-			}
-			
-			int cmp = key.compareTo(x.key);
-			if (cmp == 0) {    
-				x.value = value;
-			}
-			
-				else if (cmp < 0) {
-					x.left  = put(x.left,  key, value);
-				}	
-					else if (cmp > 0) { 
-						x.right = put(x.right, key, value); 
-					}	
-			
-			return x;
-			
-
-			
-		}
-
-		@Override
-		public void putAll(K[] array) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public V remove(K key) {
-			// TODO Auto-generated method stub
-			return ;
-		}
-
-		@SuppressWarnings("rawtypes")
-		@Override
-		public int size(Node node) {
-			if (node == null) {
-			return 0;	
-			}
-			return 1 + size(node.left) + size(node.right);
-			
-		}
-
-		@Override
-		public K update(int amount, K key) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public void forEach() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void extractAll(K[] array) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void map() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((root == null) ? 0 : root.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
+	@Override
+	public boolean containsValue(V value, Node<K, V> node) {
+		if (node == null) {
+			return false;
+		} else {
+			if (node.value == value) {
 				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			BinärHashBaum other = (BinärHashBaum) obj;
-			if (root == null) {
-				if (other.root != null)
-					return false;
-			} else if (!root.equals(other.root))
-				return false;
-			return true;
+			}
+			return containsValue(value, node.left)
+					&& containsValue(value, node.right);
+		}
+	}
+
+	@Override
+	public boolean containsKey(int hashKey, Node node) {
+		if (node == null) {
+			return false;
+		} else {
+			if (node.hashKey == hashKey) {
+				return true;
+			} else if (hashKey < node.hashKey) {
+				return containsKey(hashKey, node.left);
+			} else {
+				return containsKey(hashKey, node.right);
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public V get(K key, Node node) {
+		if (node == null) {
+			return null;
+		} else {
+			if (node.key == key) {
+				return (V) node.value;
+			} else if (key.hashCode() < node.key.hashCode()) {
+				return get(key, node.left);
+			} else {
+				return get(key, node.right);
+			}
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	@Override
+	public void put(K key, V value) {
+		root = put(root, key, value);
+	}
+
+	public Node put(Node x, K key, V value) {
+		if (x == null) {
+			return new Node(key, value);
 		}
 
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0) {
+			x.value = value;
+		}
+
+		else if (cmp < 0) {
+			x.left = put(x.left, key, value);
+		} else if (cmp > 0) {
+			x.right = put(x.right, key, value);
+		}
+
+		return x;
+
+	}
+
+	@Override
+	public void putAll(K[] array) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public V remove(K key) {
+
+		return remove(root, key);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public int size(Node node) {
+		if (node == null) {
+			return 0;
+		} 
+		if (node.left && node.right =! null){
+		return 1 + size(node.left) + size(node.right);
+
+	}
+
+	@Override
+	public K update(int amount, K key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void forEach() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void extractAll(K[] array) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void map() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((root == null) ? 0 : root.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BinärHashBaum other = (BinärHashBaum) obj;
+		if (root == null) {
+			if (other.root != null)
+				return false;
+		} else if (!root.equals(other.root))
+			return false;
+		return true;
+	}
 
 }
